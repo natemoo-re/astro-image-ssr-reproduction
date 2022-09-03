85ab238b31d6f8a9ab63417f44a876376eb2deb3
Lennart Gastler <lennart@lgastler.de>
2022-09-03 20:32:02 +0200

    chore(init): Initial commit 🚀

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+import image from "@astrojs/image";
+
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [image()],
+  output: "server",
+  adapter: netlify()
+});
