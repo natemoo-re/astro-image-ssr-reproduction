2fca8e15fb035fec887f47307891114c789b34b8
Nate Moore <nate@astro.build>
2023-08-22 14:10:04 -0500

    use assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
-import image from "@astrojs/image";
-
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image()],
+  integrations: [],
+  experimental: {
+    assets: true
+  },
   output: "server",
   adapter: netlify()
 });
